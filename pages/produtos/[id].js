98923d0f1579eaa613464dd95e4d6135f1b03a08
alexandrecamurca <alexandre.camurca@unesp.br>
2022-09-11 16:32:44 -0300

    Initial commit

--- pages/produtos/[id].js
+++ pages/produtos/[id].js
@@ -1,5 +1,31 @@
-function Produtos() {
+export async function getStaticPaths() {
+    return {
+        paths: [{
+            params: {
+                id: '1'
+            }
+        },{
+            params: {
+                id: '2'
+            }
+        }],
+        fallback: false
+    }
+}
 
+export async function getStaticProps(context) {
+    const id = context.params.id;
+
+    return {
+        props: {
+            id: id
+        }
+    }
+}
+
+function Produtos(props) {
+    
+    return <div>Id do produto: {props.id}</div>
 }
 
 export default Produtos;
