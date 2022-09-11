d3e647104256de4c2487ee7d262febc4ade4b90f
alexandrecamurca <alexandre.camurca@unesp.br>
2022-09-10 21:29:45 -0300

    Initial commit

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,9 +1,9 @@
-function Home() {
+function Sobre() {
     return <div>
-                <h1>Stocks Selector</h1>
+                <h1>Sobre</h1>
                 <p>Under Construction</p>
-                <a href="/sobre">Sobre</a>
+                <a href="/">Home</a>
             </div>
 }
 
-export default Home
+export default Sobre
