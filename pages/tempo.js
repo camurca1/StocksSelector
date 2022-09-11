62420cca38843e723b0518a3bce9e2234b42e698
alexandrecamurca <alexandre.camurca@unesp.br>
2022-09-10 22:17:12 -0300

    Initial commit

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,10 +1,24 @@
-function Tempo() {
+function Tempo(props) {
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
 
     return (
         <div>
-            {dynamicDateString}
+            <div>{dynamicDateString} (dinâmico)</div>
+            <div>{props.staticDateString} (estático)</div>
         </div>
     )
 }
+
+export function getStaticProps() {
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return {
+        props: {
+            staticDateString
+        }
+    }
+}
+
+export default Tempo;
