271b4bdbc1e242dbcb89c0ef46f0a252c93ff524
alexandrecamurca <alexandre.camurca@unesp.br>
2022-09-10 21:27:58 -0300

    Initial commit

---
+++ pages/sobre.js
@@ -0,0 +1,9 @@
+function Home() {
+    return <div>
+                <h1>Stocks Selector</h1>
+                <p>Under Construction</p>
+                <a href="/sobre">Sobre</a>
+            </div>
+}
+
+export default Home
